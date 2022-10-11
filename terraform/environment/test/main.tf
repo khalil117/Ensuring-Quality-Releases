1ff5891cc58b4824a75a9734ef1fc10e2470e59c
Khalil TOUIL <33384529+khalil117@users.noreply.github.com>
2022-10-12 01:36:21 +0200

    Update main.tf

--- terraform/environment/test/main.tf
+++ terraform/environment/test/main.tf
@@ -13,11 +13,8 @@ terraform {
     access_key           = ""
   }
 }
-module "resource_group" {
-  source               = "../../modules/resource_group"
-  resource_group       = "${var.resource_group}"
-  location             = "${var.location}"
-}
+
+
 module "network" {
   source               = "../../modules/network"
   address_space        = "${var.address_space}"
@@ -25,7 +22,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
+  resource_group       = "${data.azurerm_resource_group.test.name}"
   address_prefix_test  = "${var.address_prefix_test}"
 }
 
@@ -34,7 +31,7 @@ module "nsg-test" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${data.azurerm_resource_group.test.name}"
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
@@ -43,12 +40,12 @@ module "appservice" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${data.azurerm_resource_group.test.name}"
 }
 module "publicip" {
   source           = "../../modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${data.azurerm_resource_group.test.name}"
 }
