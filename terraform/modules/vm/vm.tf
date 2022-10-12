c015f5a4eff1db7f83db3415d22a9f1fd9b90751
Khalil TOUIL <33384529+khalil117@users.noreply.github.com>
2022-10-12 02:17:52 +0200

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_interface" "" {
+resource "azurerm_network_interface" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
